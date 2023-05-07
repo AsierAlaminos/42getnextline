ddb8783c4cf0908ddd61c1a0db4ad131fadf13dc
AsierAlaminos <alaminosasier@gmail.com>
2023-05-07 19:21:49 +0200

    update

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,34 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asmus37 <asmus37@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/04 18:58:48 by asmus37           #+#    #+#             */
-/*   Updated: 2023/05/04 18:58:49 by asmus37          ###   ########.fr       */
+/*   Created: 2023/05/07 19:02:03 by asmus37           #+#    #+#             */
+/*   Updated: 2023/05/07 19:02:04 by asmus37          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-char	*get_next_line(int fd)
+char	*get_line(char *text)
 {
-	char	*readed;
-	int	leidos;
-
-	readed = malloc(sizeof(char) * BUFFER_SIZE);
-	read(fd, readed, BUFFER_SIZE);
-	printf("texto: %s\n", readed);
-	return (readed);
-}
-
-int	main(void)
-{
-	int	fd = open("./test", O_RDONLY);
-	get_next_line(fd);
-	return (0);
+	int	
 }
