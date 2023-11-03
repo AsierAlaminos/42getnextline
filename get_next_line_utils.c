90375de759529bf2db500e576ac561dbabc95a66
Asier <asmus@localhost.localdomain>
2023-11-03 18:00:26 +0100

    correccion leaks

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -54,7 +54,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	int		j;
 
 	pointer = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (pointer == NULL)
+	if (!pointer)
 		return (NULL);
 	i = 0;
 	while (s1[i] != '\0')
